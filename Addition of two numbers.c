517eeeaa63dd50b2c96d18facbe2bce91b221faa
adhinkrishna2255-source <adhinkrishna2255@gmail.com>
2025-11-02 22:16:17 +0530

    Add files via upload

---
+++ Addition of two numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Enter a");
+    scanf("%d",&a);
+
+    int b;
+    printf("Enter b");
+    scanf("%d",&b);
+
+    printf("Sum of a and b is : %d",a+b);
+    return 0;
+}
+

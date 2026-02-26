764f38587d5b9e83c4057ec5e26ae1c665fb9168
adhinkrishna2255-source <adhinkrishna2255@gmail.com>
2026-02-26 11:34:48 +0530

    Add files via upload

---
+++ Proram to Demonstrate Conditional Compilation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10,b = 5,sum;
+    sum = a+b;
+    #ifdef DEBUG
+      printf("Debug: a = %d,b = %d\n",a,b);
+    #endif // DEBUG
+    printf("Sum = %d\n",sum);
+    return 0;
+}
